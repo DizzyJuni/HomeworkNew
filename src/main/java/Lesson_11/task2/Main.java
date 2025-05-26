c23f6a7cce632417c736610043430ff8b4049e71
DizzyJuni <D1zzyjuni@gmail.com>
2025-05-25 18:38:52 -0700

    Убрал лишние методы не соответсвующие условию задания для нахождения суммы чисел. Поиск гласных через HashSet. Поиск в строке только букв

--- src/main/java/Lesson_11/task2/Main.java
+++ src/main/java/Lesson_11/task2/Main.java
@@ -5,21 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(Arrays.toString(twoSum(nums, 10)));
         System.out.println(Arrays.toString(twoSumMap(nums, 19)));
-        System.out.println(Arrays.toString(twoSumSet(nums, 19)));
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
-        for (int i : nums) {
-            for (int j : nums) {
-                if (i + j == target) {
-                    System.out.println("Числа в массиве");
-                    return new int[]{i, j};
-                }
-            }
-        }
-        throw new IndexNotFound("Нет чисел в массиве для получения суммы " + target);
     }
 
     public static int[] twoSumMap(int[] nums, int target) {
@@ -31,19 +17,6 @@ public class Main {
                 return new int[]{indexMap.get(x), i};
             }
             indexMap.put(nums[i], i);
-        }
-        throw new IndexNotFound("Нет чисел в массиве для получения суммы " + target);
-    }
-
-    public static int[] twoSumSet(int[] nums, int target) {
-        Set<Integer> numSet = new HashSet<>();
-        for (int num : nums) {
-            int x = target - num;
-            if (numSet.contains(x)) {
-                System.out.println("Числа в массиве");
-                return new int[]{x, num};
-            }
-            numSet.add(num);
         }
         throw new IndexNotFound("Нет чисел в массиве для получения суммы " + target);
     }
